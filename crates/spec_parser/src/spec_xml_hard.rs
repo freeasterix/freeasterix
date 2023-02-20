5847c2a730194a5a9bcac2cd6dd076fd2083596e
Igor Malovitsa <igor.malovitsa@gmail.com>
2023-02-20 09:29:05 +0200

    Added specs and specs parsers

---
+++ crates/spec_parser/src/spec_xml_hard.rs
@@ -0,0 +1,305 @@
+//! The purpose of this module is to provide ASTERIX XML -> Rust deserialization
+//! of XML specs, produced by [CroatiaControlLtd/asterix](https://github.com/CroatiaControlLtd/asterix).
+//! XML root item is [`Category`](Category)
+
+// TODO: this lint originates from hard_xml, maybe fix it?
+#![allow(clippy::needless_late_init)]
+
+use crate::unit::Unit;
+use hard_xml::{XmlRead, XmlWrite};
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Category")]
+pub struct Category {
+    #[xml(attr = "id")]
+    pub id: u64,
+    #[xml(attr = "name")]
+    pub name: String,
+    #[xml(attr = "ver")]
+    pub ver: String,
+    #[xml(child = "DataItem")]
+    pub data_items: Vec<DataItem>,
+    #[xml(child = "UAP")]
+    pub uaps: Vec<UAP>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "DataItem")]
+pub struct DataItem {
+    #[xml(attr = "id")]
+    pub id: String,
+    #[xml(attr = "rule")]
+    pub rule: Option<Rule>,
+    #[xml(flatten_text = "DataItemName")]
+    pub name: String,
+    #[xml(flatten_text = "DataItemDefinition")]
+    pub definition: String,
+    #[xml(child = "DataItemFormat")]
+    pub format: DataItemFormat,
+    #[xml(flatten_text = "DataItemNote")]
+    pub note: Option<String>,
+}
+
+#[derive(Debug, Default)]
+pub enum Rule {
+    Mandatory,
+    Optional,
+    #[default]
+    Unknown,
+}
+
+impl std::str::FromStr for Rule {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use Rule::*;
+        let rv = match s {
+            "mandatory" => Mandatory,
+            "optional" => Optional,
+            "unknown" => Unknown,
+            _ => return Err("bad DataItem rule attribute"),
+        };
+        Ok(rv)
+    }
+}
+
+impl std::fmt::Display for Rule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        use Rule::*;
+        let s = match self {
+            Mandatory => "mandatory",
+            Optional => "optional",
+            Unknown => "unknown",
+        };
+        f.write_str(s)
+    }
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "DataItemFormat")]
+pub struct DataItemFormat {
+    #[xml(attr = "desc")]
+    pub desc: Option<String>,
+    #[xml(
+        child = "Fixed",
+        child = "Explicit",
+        child = "Repetitive",
+        child = "Variable",
+        child = "Compound",
+        child = "BDS"
+    )]
+    pub formats: Vec<Format>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[allow(clippy::upper_case_acronyms)]
+pub enum Format {
+    #[xml(tag = "Fixed")]
+    Fixed(Fixed),
+    #[xml(tag = "Variable")]
+    Variable(Variable),
+    #[xml(tag = "Explicit")]
+    Explicit(Explicit),
+    #[xml(tag = "Repetitive")]
+    Repetitive(Repetitive),
+    #[xml(tag = "Compound")]
+    Compound(Compound),
+    #[xml(tag = "BDS")]
+    BDS,
+}
+
+// TODO(igorm): is it possible to make hard_xml run in strict mode where
+// unexpected tags return an error?  Currently unexpected tags are skipped.
+// The purpose of this code is to create an intentionally broad child matching
+// in formats, and to check whether stricter definitions of format tags
+// Variable/Explicit/Repetitive/Compound do not miss any data.
+/*
+macro_rules! make_wrap {
+    ($tag:expr, $name:ident) => {
+        #[derive(XmlRead, Debug)]
+        #[xml(tag=$tag)]
+        pub struct $name {
+            #[xml(
+                child = "Fixed",
+                child = "Compound",
+                child = "Explicit",
+                child = "Repetitive",
+                child = "Variable",
+            )]
+            pub formats: Vec<Format>,
+        }
+    };
+}
+
+make_wrap!("Variable", Variable);
+make_wrap!("Explicit", Explicit);
+make_wrap!("Repetitive", Repetitive);
+make_wrap!("Compound", Compound);
+*/
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Variable")]
+pub struct Variable {
+    #[xml(child = "Fixed")]
+    pub formats: Vec<Format>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Explicit")]
+pub struct Explicit {
+    #[xml(
+        child = "Fixed",
+        child = "Explicit",
+        child = "Repetitive",
+        child = "Variable",
+        child = "Compound"
+    )]
+    pub formats: Vec<Format>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Repetitive")]
+pub struct Repetitive {
+    #[xml(child = "Fixed", child = "BDS")]
+    pub formats: Vec<Format>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Compound")]
+pub struct Compound {
+    #[xml(
+        child = "Variable",
+        child = "Fixed",
+        child = "Repetitive",
+        child = "Compound"
+    )]
+    pub formats: Vec<Format>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Fixed")]
+pub struct Fixed {
+    #[xml(attr = "length")]
+    pub length: u64,
+    #[xml(child = "Bits")]
+    pub bits: Vec<Bits>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "Bits")]
+pub struct Bits {
+    #[xml(attr = "bit")]
+    pub bit: Option<u64>,
+    #[xml(attr = "from")]
+    pub from: Option<u64>,
+    #[xml(attr = "to")]
+    pub to: Option<u64>,
+    #[xml(attr = "fx")]
+    pub fx: Option<u64>,
+    #[xml(attr = "rep")]
+    pub rep: Option<u64>,
+    #[xml(attr = "encode")]
+    pub encode: Option<Encode>,
+    #[xml(flatten_text = "BitsShortName")]
+    pub short_name: String,
+    #[xml(flatten_text = "BitsConst")]
+    pub cons: Option<String>,
+    #[xml(flatten_text = "BitsName")]
+    pub name: Option<String>,
+    #[xml(child = "BitsValue")]
+    pub values: Vec<BitsValue>,
+    #[xml(child = "BitsUnit")]
+    pub unit: Option<BitsUnit>,
+    #[xml(flatten_text = "BitsPresence")]
+    pub presence: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum Encode {
+    Unsigned,
+    Signed,
+    Octal,
+    Hex,
+    SixBitsChar,
+    Ascii,
+}
+
+impl std::str::FromStr for Encode {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use Encode::*;
+        let rv = match s {
+            "unsigned" => Unsigned,
+            "signed" => Signed,
+            "octal" => Octal,
+            "hex" => Hex,
+            "6bitschar" => SixBitsChar,
+            "ascii" => Ascii,
+            _ => return Err("bad Bits encode attr"),
+        };
+        Ok(rv)
+    }
+}
+
+impl std::fmt::Display for Encode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        use Encode::*;
+        let s = match self {
+            Unsigned => "unsigned",
+            Signed => "signed",
+            Octal => "octal",
+            Hex => "hex",
+            SixBitsChar => "6bitschar",
+            Ascii => "ascii",
+        };
+        f.write_str(s)
+    }
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "BitsValue")]
+pub struct BitsValue {
+    #[xml(attr = "val")]
+    pub val: u64,
+    #[xml(text)]
+    pub desc: String,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "BitsUnit")]
+pub struct BitsUnit {
+    #[xml(attr = "min")]
+    pub min: Option<f64>,
+    #[xml(attr = "max")]
+    pub max: Option<f64>,
+    #[xml(attr = "scale")]
+    pub scale: Option<f64>,
+    #[xml(text)]
+    pub unit: Unit,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "UAP")]
+#[allow(clippy::upper_case_acronyms)]
+pub struct UAP {
+    #[xml(attr = "use_if_bit_set")]
+    pub use_if_bit_set: Option<u64>,
+    #[xml(attr = "use_if_byte_nr")]
+    pub use_if_byte_nr: Option<u64>,
+    #[xml(attr = "is_set_to")]
+    pub is_set_to: Option<u8>,
+    #[xml(child = "UAPItem")]
+    pub items: Vec<UAPItem>,
+}
+
+#[derive(XmlRead, XmlWrite, Debug)]
+#[xml(tag = "UAPItem")]
+pub struct UAPItem {
+    #[xml(attr = "bit")]
+    pub bit: u64,
+    #[xml(attr = "frn")]
+    pub frn: String,
+    #[xml(attr = "len")]
+    pub len: Option<String>,
+    #[xml(text)]
+    pub name: String,
+}
---
+++ crates/spec_parser/src/unit.rs
@@ -0,0 +1,194 @@
+//! List of all units of measurements used in ASTERIX protocol.
+
+/// Unit of measurement
+#[derive(Debug)]
+pub enum Unit {
+    /// Unitless measure
+    Unitless,
+    /// Percent, 1 part per 100
+    Percent,
+    /// Second, unit of time
+    Second,
+    /// Milliseconds, 1**10-3 second
+    Millisecond,
+    /// Nanoseconds, 1**10-9 second
+    NanoSecond,
+    /// Femtoseconds, 1**10-15 second
+    FemtoSecond,
+    /// Degree (angle)
+    Degree,
+    /// Degree per second (angular speed)
+    DegreePerSecond,
+    /// Degree Celsius (temperature)
+    Celsius,
+    /// Flight Level, unit of altitude (expressed in 100's of feet)
+    FlightLevel,
+    /// MegaHertz
+    MHz,
+    /// Meter
+    Meter,
+    /// Meter per second
+    MeterPerSecond,
+    /// Meter per second squared, acceleration
+    MeterPerSecondSq,
+    /// Mach number
+    Mach,
+    /// Nautical Mile, 1852 meters, or 6076 feet
+    NauticalMile,
+    /// Knot, 1 nautical mile/hour, or 1.852 kilometers / hour
+    Knot,
+    /// Nautical mile per second, or 1852 meters / second
+    NauticalMilePerSecond,
+    /// Feet, 0.3048 meters
+    Feet,
+    /// Feet per minute
+    FeetPerMinute,
+    /// Hectopascal, 100 Pascal
+    HectoPascal,
+    /// Millibar, 1*10-3 Bar, 100 Pascal
+    Millibar,
+    /// The dB is the unit of relative power
+    #[allow(non_camel_case_types)]
+    dB,
+    /// The dBm is the unit of absolute power related to 1 milliwatt
+    #[allow(non_camel_case_types)]
+    dBm,
+    /// The dBuV is the unit of absolute power related to 1 microvolt.
+    /// `P_{dBm} = E_{dBuV} - 95.2dB`
+    /// Only met in XML specs.
+    #[allow(non_camel_case_types)]
+    dBuV,
+    /// Minute, XML-spec specific
+    Minute,
+    /// Kilobytes per second, XML-spec specific
+    KBytesPerSecond,
+}
+
+use std::str::FromStr;
+impl FromStr for Unit {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use Unit::*;
+        // Note: cat008 contains notes inside units ¯\_(ツ)_/¯
+        let s = s.split(" /Note:").next().unwrap();
+        let rv = match s {
+            "" => Unitless,
+            "%" => Percent,
+            "s" => Second,
+            "ms" => Millisecond,
+            "ns" => NanoSecond,
+            "fs" => FemtoSecond,
+            "°" => Degree,
+            "°/s" => DegreePerSecond,
+            "°C" => Celsius,
+            "FL" => FlightLevel,
+            "MHz" => MHz,
+            "m" => Meter,
+            "m/s" => MeterPerSecond,
+            "m/s2" => MeterPerSecondSq,
+            "Mach" => Mach,
+            "NM" => NauticalMile,
+            "kt" => Knot,
+            "NM/s" => NauticalMilePerSecond,
+            "ft" => Feet,
+            "ft/min" => FeetPerMinute,
+            "hPa" => HectoPascal,
+            "mb" => Millibar,
+            "dB" => dB,
+            "dBm" => dBm,
+            // Note: special case for XML spec, which has scaling factor
+            // embedded in the unit for some reason.
+            "*2^(-6+f)" => NauticalMile,
+            "* 2^(-6+f)" => NauticalMile,
+            "* 2^(-7+f)" => NauticalMile,
+            // Units from obsolete specs
+            // "/1000" => Unitless,
+            // XML-specific unit names, because XML spec isn't consistent
+            // TODO: what's up with these gain units in XML spec?
+            "*10E-5" => Unitless,
+            "dBuV" => dBuV,
+            "min" => Minute,
+            "KBytes/s" => KBytesPerSecond,
+            "sec" => Second,
+            "deg" => Degree,
+            "deg." => Degree,
+            "deg/s" => DegreePerSecond,
+            "deg/sec" => DegreePerSecond,
+            "C" => Celsius,
+            "deg C" => Celsius,
+            "m/sec" => MeterPerSecond,
+            "m/s^2" => MeterPerSecondSq,
+            "m/s²" => MeterPerSecondSq,
+            "Nm" => NauticalMile,
+            "knot" => Knot,
+            "feet/minute" => FeetPerMinute,
+            "mbar" => Millibar,
+            _ => return Err(format!("unknown unit `{s}`")),
+        };
+        Ok(rv)
+    }
+}
+
+impl Unit {
+    fn to_str(&self) -> &'static str {
+        use Unit::*;
+        match self {
+            Unitless => "",
+            Percent => "%",
+            Second => "s",
+            Millisecond => "ms",
+            NanoSecond => "ns",
+            FemtoSecond => "fs",
+            Degree => "°",
+            DegreePerSecond => "°/s",
+            Celsius => "°C",
+            FlightLevel => "FL",
+            MHz => "MHz",
+            Meter => "m",
+            MeterPerSecond => "m/s",
+            MeterPerSecondSq => "m/s2",
+            Mach => "Mach",
+            NauticalMile => "NM",
+            NauticalMilePerSecond => "NM/s",
+            Feet => "ft",
+            FeetPerMinute => "ft/min",
+            Knot => "kt",
+            HectoPascal => "hPa",
+            Millibar => "mb",
+            dB => "dB",
+            dBm => "dBm",
+            // XML specific
+            Minute => "min",
+            dBuV => "dBuV",
+            KBytesPerSecond => "KBytes/s",
+        }
+    }
+}
+
+use std::fmt;
+impl fmt::Display for Unit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.to_str())
+    }
+}
+
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+impl Serialize for Unit {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_str())
+    }
+}
+
+impl<'de> Deserialize<'de> for Unit {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let val = Unit::from_str(&s).map_err(de::Error::custom)?;
+        Ok(val)
+    }
+}
